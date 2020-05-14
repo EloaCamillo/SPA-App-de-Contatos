83c8a42fe4074e533baf603e449a34a9b9e0a678
eloacamillo <eloa.camillo@going2.com.br>
2020-05-14 18:08:34 -0300

    Modulzarizando componentes e chamda da api

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.scss';
+import Topbar from './components/Topbar';
+import Filter from './components/Filters';
+import Contacts from './components/Contacts';
+import Contact from './components/Contact';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contact: []
+    }
+  }
+  componentDidMount() {
+    const url = "https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts"
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(data => {
+        this.setState({
+          contact: data
+
+        })
+        console.log("teste", this.state.contact)
+      }).catch((error) => {
+        console.log("Error!" + error)
+      })
+
+  }
+  render() {
+    console.log("render")
+    return (
+      <React.Fragment>
+
+        <Topbar />
+        <Filter />
+        <Contacts>
+        <Contact />
+        </Contacts>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
